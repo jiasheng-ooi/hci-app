f56bcf0c7ecbbc9539c741dd71bc94124aab63d6
Faith Lim <user@Faiths-MacBook-Air.local>
2023-07-14 14:31:03 +0800

    initial FE setup

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: ["./src/**/*.js", "./src/**/*.jsx", "./src/**/*.ts", "./src/**/*.tsx"],
+  theme: {
+    
+    extend: {
+     
+    },
+  },
+  plugins: [],
+}
