5c13cc769e1d0fd76d9718bc40c5d55f90e5fac3
jiasheng-ooi <95675395+jiasheng-ooi@users.noreply.github.com>
2023-07-16 00:38:54 +0800

    fuxed  login

--- src/views/login.jsx
+++ src/views/login.jsx
@@ -1,69 +1,34 @@
-    import React from 'react';
-    import { FcGoogle } from 'react-icons/fc';
-    import { Button } from '@mantine/core';
-    import backgroundImage from '../assets/login.jpg'; // Update the import path based on your folder structure
+import React from 'react';
+import { FcGoogle } from 'react-icons/fc';
+import { Button } from '@mantine/core';
+import backgroundImage from '../assets/login.jpg'; // Update the import path based on your folder structure
 
-    const LoginPage = () => {
-    const handleGoogleLogin = () => {
-        // Perform login with Google logic here
-        console.log('Login with Google');
-    };
+const LoginPage = () => {
+  const handleGoogleLogin = () => {
+    // Perform login with Google logic here
+    console.log('Login with Google');
+  };
 
-    return (
-        <div
-        className="min-h-screen flex justify-center items-center p-8"
-        style={{
-            backgroundImage: `url(${backgroundImage})`,
-            backgroundSize: 'cover',
-            backgroundPosition: 'center',
-            width: 720,
-            height: 1280,
-            display: 'flex',
-            flexDirection: 'column',
-            alignItems: 'center',
-        }}
+  return (
+    <div
+      className="min-h-screen w-screen flex justify-center items-center bg-cover"
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        height: 1280,
+      }}
+    >
+      <div className="text-center flex flex-col items-center">
+        <h1 className="text-4xl font-bold mb-6 text-white">ParkFinder.</h1>
+        <Button
+          className="w-72 h-16 bg-white text-teal-950 text-xl font-semibold rounded-full text-lg flex items-center justify-center transform transition-transform hover:scale-110 hover: border-2 border-black"
+          onClick={handleGoogleLogin}
         >
+          <FcGoogle className="mr-3 text-3xl" />
+          Log In with Google
+        </Button>
+      </div>
+    </div>
+  );
+};
 
-        <div className="text-center flex-col" style={{ marginTop: '30vh' }}>
-        <h1
-          style={{
-            fontSize: '3rem', // Equivalent to text-4xl
-            fontWeight: 'bold', // Equivalent to font-bold
-            marginBottom: '1.5rem', // Equivalent to mb-6
-            color: 'white', // Equivalent to text-white
-            textAlign: 'center',
-
-          }}
-        >
-          ParkFinder.
-        </h1>
-            <Button
-            style={{
-                backgroundColor: 'white',
-                color: 'navy',
-                border: '1px solid black',
-                borderRadius: '9999px',
-                width: '15em', // Adjust the width to control the scale of the button
-                height: '3em', // Adjust the height to control the scale of the button
-                fontSize: '1.6em', // Scaled the font size
-                display: 'flex',
-                alignItems: 'center',
-                justifyContent: 'center',
-                padding: '0.5em 1em', // Added padding to create a gap between the icon and text
-
-            }}
-            onMouseOver={(e) => (e.target.style.borderWidth = '2px')}
-            onMouseOut={(e) => (e.target.style.borderWidth = '1px')}
-            onClick={handleGoogleLogin}
-            >
-            <span style={{ fontSize: '1.6em', paddingRight: '0.5em' }}>
-            <FcGoogle />
-          </span>
-            Log In with Google
-            </Button>
-        </div>
-        </div>
-    );
-    };
-
-    export default LoginPage;
+export default LoginPage;
--- src/views/onboard.jsx
+++ src/views/onboard.jsx
@@ -1,6 +1,4 @@
 import React, { useState } from 'react';
-import backgroundImage from '../assets/intro.jpg'; // Update the import path based on your folder structure
-
 
 const OnboardingPage = () => {
   const [name, setName] = useState('');
@@ -13,28 +11,36 @@ const OnboardingPage = () => {
   };
 
   return (
-    <div className="container mx-auto px-4 max-w-sm">
-      <h1 className="text-3xl font-bold mb-6">Onboarding Page</h1>
-      <form className="space-y-4" onSubmit={handleFormSubmit}>
-        <div>
-          <label htmlFor="name" className="block font-medium">
-            Name
-          </label>
-          <input
-            type="text"
-            id="name"
-            value={name}
-            onChange={(e) => setName(e.target.value)}
-            className="block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
-          />
-        </div>
-        <button
-          type="submit"
-          className="w-full px-4 py-2 bg-blue-500 text-white rounded-md font-semibold hover:bg-blue-600"
-        >
-          Complete Onboarding
-        </button>
-      </form>
+    <div
+      className="min-h-screen flex justify-center items-center p-8"
+      style={{
+        width: 720,
+        height: 1280,
+      }}
+    >
+      <div className="text-center">
+        <h1 className="text-4xl font-bold mb-6">Onboarding Page</h1>
+        <form className="space-y-4" onSubmit={handleFormSubmit}>
+          <div>
+            <label htmlFor="name" className="block font-medium">
+              Name
+            </label>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              className="block w-full px-4 py-2 border border-gray-300 rounded-md shadow-sm focus:ring-blue-500 focus:border-blue-500"
+            />
+          </div>
+          <button
+            type="submit"
+            className="w-full px-4 py-2 bg-blue-500 text-white rounded-md font-semibold hover:bg-blue-600"
+          >
+            Complete Onboarding
+          </button>
+        </form>
+      </div>
     </div>
   );
 };
